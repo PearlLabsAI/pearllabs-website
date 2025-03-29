18ab291888edab8762e4257c469c1f0f2a63a657
Matt Falcioni <mfalcioni@brunnerworks.com>
2025-03-29 16:25:32 -0400

    Updated the logo in Navigation and added some other logo assets

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { Menu, X } from 'lucide-react';
 import { useState } from 'react';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo_only_no_backdrop.svg';
 
 
 export default function Navbar() {
@@ -12,7 +11,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex items-center">
-            <img src={logo} width={25} height={25} alt="Pearl Labs LLC logo"/>
+            <img src={logo} width={56} height={56} alt="Pearl Labs LLC logo"/>
             <span className="ml-2 text-xl font-bold text-gray-900">Pearl Labs AI Solutions</span>
           </div>
           
